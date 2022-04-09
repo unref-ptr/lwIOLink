412e40e00eb56bff3de09f2ed4d226e124a7658f
unref-ptr <81707041+unref-ptr@users.noreply.github.com>
2022-04-09 14:20:34 +0200

    minor refactoring

--- src/lwIOLink.hpp
+++ src/lwIOLink.hpp
@@ -32,47 +32,47 @@ class lwIOLink
     //Modes for the Device
     enum Mode
     {
-	start,
-	preoperate,
-	operate
+        start,
+        preoperate,
+        operate
     };
-    enum BaudRate
+    enum BaudRate: uint32_t
     {
-	COM1 = 4800,
-	COM2 = 38400,
-	COM3 = 230400
+        COM1 = 4800,
+        COM2 = 38400,
+        COM3 = 230400
     };
     //Table A.5
     enum PDStatus
     {
-	Valid = 0,
-	Invalid = 1
+        Valid = 0,
+        Invalid = 1
     };
-    struct Config_t
+    struct HWConfig
     {
-	Stream &serial; 		 //reference to Serial port
-	BaudRate baud;			// baud rate for transmission
-	int WakeupMode; 	/* Type of interrupt to detect wakeup from transceiver   
-		   			FALLING, RISING */
-	struct Pin_t
-	{
-		unsigned TxEN;  	 //Digital Output pin used to enable TX of data
-		unsigned Wakeup;	/* Digital Input pin used to get Wakeup Requests
-		  			Must be interrupt capable. */
+        Stream &SerialPort; 		 //reference to Arduino Serial port
+        BaudRate Baud;			// baud rate for transmission
+        int WakeupMode; 	/* Type of interrupt to detect wakeup from transceiver   
+                        FALLING, RISING */
+        struct Pin_t
+        {
+            unsigned TxEN;  	 //Digital Output pin used to enable TX of data
+            unsigned Wakeup;	/* Digital Input pin used to get Wakeup Requests
+                        Must be interrupt capable. */
 #ifdef ARDUINO_ARCH_ESP32
-		/* Most ESP32 Dev kits dont have default UART
-		  pins exposed. Hence need to configure available pins                   
-		*/
-		unsigned Tx;	
-		unsigned Rx;
+            /* Most ESP32 Dev kits dont have default UART
+              pins exposed. Hence need to configure available pins                   
+            */
+            unsigned Tx;	
+            unsigned Rx;
 #endif //ARDUINO_ARCH_ESP32
-	};
-	Pin_t Pin;
+        };
+        Pin_t Pin;
     };
     /*
        Begin the IOLink Device Hardware
     */
-    void begin(const Config_t config);
+    void begin(const HWConfig config);
     /*
        Run the IOLink Device, should be called in a loop
     */
@@ -115,106 +115,106 @@ class lwIOLink
     static unsigned constexpr TotalTimeEncodings = 3;
     static uint32_t constexpr TimeBaseLUT[TotalTimeEncodings] = {100, 400, 1600}; //us
     static uint32_t constexpr TimeOffsetLUT[TotalTimeEncodings] = {0,6400,32000}; //us
-    
+
     //ISDU Not implemented
     static bool constexpr ISDUSupported = false;
     //Message struct for IO-Link
     struct ioLink_message_t
     {
-	uint8_t channel;
-	uint8_t addr;
+        uint8_t channel;
+        uint8_t addr;
     };
 
     enum Channel
     {
-	PROCESS = 0,
-	PAGE ,
-	DIAGNOSIS,
-	ISDU
+        PROCESS = 0,
+        PAGE ,
+        DIAGNOSIS,
+        ISDU
     };
     struct ODSize_t
     {
-	uint8_t Startup;
-	uint8_t Preop;
-	uint8_t Op;
+        uint8_t Startup;
+        uint8_t Preop;
+        uint8_t Op;
     };
     //OD Size for the device (1,2,8 or 32)
     static ODSize_t constexpr ODSize =
     {
-	.Startup = 1,
-	.Preop = 8,
-	.Op = 2
+        .Startup = 1,
+        .Preop = 8,
+        .Op = 2
     };
     //Table A.2
     enum class MCAccess
     {
-	Read = 1,
-	Write = 0
+        Read = 1,
+        Write = 0
     };
 
     //Table A.3
     enum class MSeqType
     {
-	Type0 = 0,
-	Type1,
-	Type2
+        Type0 = 0,
+        Type1,
+        Type2
     };
     //States for interpreting IO-Link data
     enum ioLink_states
     {
-	wait_wake, //Waiting for wakeup signal
-	wait_valid_frame, //Waiting for a valid startup message
-	run_mode  //Master started communication with device
+        wait_wake, //Waiting for wakeup signal
+        wait_valid_frame, //Waiting for a valid startup message
+        run_mode  //Master started communication with device
     };
 
     //Table B.2
     enum MasterCommands
     {
-	MasterIdent = 0x95,
-	DeviceIden = 0x96,
-	DeviceStartup = 0x97,
-	PDOutOperate = 0x98,
-	Operate = 0x99,
-	Preoperate = 0x9A,
+        MasterIdent = 0x95,
+        DeviceIden = 0x96,
+        DeviceStartup = 0x97,
+        PDOutOperate = 0x98,
+        Operate = 0x99,
+        Preoperate = 0x9A,
     };
     /* Table B.1 */
     enum DP1_Param
     {
-	MasterCommand = 0x00,
-	MasterCycleTime,
-	MinCycleTime,
-	MSeqCap,
-	RevisionID,
-	ProcessDataIn,
-	ProcessDataOut,
-	VID1,
-	VID2,
-	VID3,
-	DID1,
-	DID2,
-	DID3,
-	FID1,
-	FID2,
-	Res,
-	SystemCommand
+        MasterCommand = 0x00,
+        MasterCycleTime,
+        MinCycleTime,
+        MSeqCap,
+        RevisionID,
+        ProcessDataIn,
+        ProcessDataOut,
+        VID1,
+        VID2,
+        VID3,
+        DID1,
+        DID2,
+        DID3,
+        FID1,
+        FID2,
+        Res,
+        SystemCommand
     };
-  
+
     struct Status
     {
-	PDStatus PDIn;
-	PDStatus PDOut;
+        PDStatus PDIn;
+        PDStatus PDOut;
     };
 
     struct PDBuffer
     {
-	uint8_t Data[MaxPD];
-	size_t Size;
+        uint8_t Data[MaxPD];
+        size_t Size;
     };
 
     struct PD
     {
-	PDBuffer Out;
-	PDBuffer In;
+        PDBuffer Out;
+        PDBuffer In;
     };
     uint8_t GetMasterTXSize();
     //Reset the RX Line
@@ -242,13 +242,16 @@ class lwIOLink
     void SaveMasterFrame(uint8_t rx_byte);
     //Generate IOLink checksum
     uint8_t GetChecksum(uint8_t *data, uint8_t length, PDStatus status) const;
-    //Decode cycletime - Table B.3
+    //Decode cycletime according to Table B.3, return value time in microseconds
     uint32_t DecodeCycleTime(uint8_t encoded_time) const;
-    //Encode cycletime - Table B.3
+    /* Encode cycletime according to Table B.3
+    *  If the cycle time time cannot be encoded 
+    *  the nearest greater cycle time will be used.
+    *  e.g., cycleTime_us = 1333 us -> encoded value = 1340 us
+    */
     uint8_t EncodeCycleTime(uint32_t cycleTime_us) const;
-    Stream * _serial;
+    Stream * SerialPort = nullptr; 
     ioLink_message_t message;
-
     PD Pd;
     uint8_t ODBuffer[MaxOD];
     /* Process Data status */
@@ -256,8 +259,8 @@ class lwIOLink
     /* Table B.1 */
     uint8_t ParameterPage1[SizeDP1] = {0};
     bool NewCmd = false;
-    uint8_t TxEn; //Enable Pin
-    uint8_t WuPin; //Wakeup Pin
+    unsigned TxEn;
+    unsigned WuPin;
     MasterCommands Cmd;
     Mode deviceMode = start;
     MCAccess MasterAccess;
